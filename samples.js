450e71311e7a4e16f2ffa4868637f16319bcf3ac
Ivan Tkachuk <tkachukivan93@gmail.com>
2017-06-27 13:58:24 +0300

    update authorization, add test

--- samples.js
+++ samples.js
@@ -1,12 +1,12 @@
-const PDFfiller = require('./index.js');
+const PDFfiller = require('./index.js')();
+const config = require('./config.json');
 
-const pdffillerInstance = new PDFfiller();
-pdffillerInstance.authorization({
-  grant_type: 'password',
-  client_id: 'client_id',
-  client_secret: 'client_secret',
-  username: 'username@mail.com',
-  password: 'user password'
+PDFfiller.authorization({
+  grant_type: config.grant_type,
+  client_id: config.client_id,
+  client_secret: config.client_secret,
+  username: config.username,
+  password: config.password
 }).then(() => {
-  pdffillerInstance.getApplication().then(data => console.log(data), err => console.log(err));
+  PDFfiller.getTokens().then(data => console.log(data), err => console.log(err));
 }, err => console.log(err));
---
+++ test/PDFfiller.js
@@ -0,0 +1,91 @@
+const should = require('should'); // eslint-disable-line
+const sinon = require('sinon');
+require('should-sinon');
+
+const PDFfiller = require('../index.js')();
+const config = require('../config.json');
+
+const mockToken = 'mocksecuretoken';
+
+describe('PDFfiller', () => {
+  describe('get and set token', () => {
+    it('should set token', () => {
+      PDFfiller.setAccessToken(mockToken);
+      PDFfiller.token.should.be.equal(mockToken);
+    });
+    it('should get token', () => {
+      PDFfiller.getAccessToken().should.be.equal(mockToken);
+    });
+  });
+
+  describe('authorization', () => {
+    const autoUpdateTokenStub = sinon.stub(PDFfiller.api, 'autoUpdateToken');
+
+    it('shouldn`t call autoUpdateToken, when autoUpdate false', (done) => {
+      PDFfiller.autoTokenUpdate = false;
+
+      PDFfiller.authorization({
+        grant_type: config.grant_type,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        username: config.username,
+        password: config.password
+      }).then(() => {
+        autoUpdateTokenStub.should.not.be.called();
+        done();
+      });
+    });
+
+    it('should call autoUpdateToken, when autoUpdate true', (done) => {
+      PDFfiller.autoTokenUpdate = true;
+      PDFfiller.authorization({
+        grant_type: config.grant_type,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        username: config.username,
+        password: config.password
+      }).then((data) => {
+        autoUpdateTokenStub.should.be.calledWith(data);
+        done();
+      });
+    });
+
+    it('should return a Promise', () => {
+      PDFfiller.authorization({
+        grant_type: config.grant_type,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        username: config.username,
+        password: config.password
+      }).should.be.a.Promise();
+    });
+
+    it('should have token data in promise', (done) => {
+      PDFfiller.authorization({
+        grant_type: config.grant_type,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        username: config.username,
+        password: config.password
+      }).then((data) => {
+        data.should.have.properties(['token_type', 'expires_in', 'access_token', 'refresh_token']);
+        done();
+      });
+    });
+
+    it('should set token after success authorization', (done) => {
+      const setAccessTokenSpy = sinon.spy(PDFfiller, 'setAccessToken');
+
+      PDFfiller.authorization({
+        grant_type: config.grant_type,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        username: config.username,
+        password: config.password
+      }).then((data) => {
+        setAccessTokenSpy.should.be.calledWith(data.access_token);
+        done();
+      });
+    });
+  });
+});
